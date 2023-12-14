6ddcdf7b479bd9714ac558f227ed418b4acca4e4
Peter Gal <153226025+petergaldata@users.noreply.github.com>
2023-12-14 12:28:46 +0100

    analytics sql separated in sql file

--- booking_analysis.py
+++ booking_analysis.py
@@ -34,6 +34,11 @@ def read_airports(data_path):
     airports = airports.toDF(*column_names)
     return airports
 
+def execute_sql_analysis(analysis_sql_path):
+    with open(analysis_sql_path, "r") as file:
+        sql_query = file.read()
+    result = spark.sql(sql_query)
+    return result
 
 if __name__ == "__main__":
     bookings_path = "data/bookings/booking.json"
@@ -43,58 +48,10 @@ if __name__ == "__main__":
     airports_path = "data/airports/airports.dat"
     airports = read_airports(airports_path)
     airports.createOrReplaceTempView("airports")
+    analysis_sql_path = "booking_analysis.sql"
 
-    result = spark.sql(
-        """
-        WITH confirmed_date_localized_bookings as(
-            SELECT 
-            b.*, 
-            from_utc_timestamp(b.departure_date, a.tz) AS departure_date_local
-            FROM bookings b
-            INNER JOIN (
-                SELECT passenger_uci, MAX(timestamp) as max_timestamp
-                FROM bookings
-                GROUP BY passenger_uci
-            ) b_max ON b.passenger_uci = b_max.passenger_uci AND b.timestamp = b_max.max_timestamp
-            INNER JOIN airports a on b.origin_airport = a.iata
-            WHERE b.booking_status = 'CONFIRMED' AND b.operating_airline = 'KL'
-        )
-        SELECT 
-        ap_dest.country AS destination_country,
-        DATE_FORMAT(to_timestamp(cb.departure_date_local), 'E') AS day_of_week, 
-        CASE 
-            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (12, 1, 2) THEN 'Winter'
-            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (3, 4, 5) THEN 'Spring'
-            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (6, 7, 8) THEN 'Summer'
-            ELSE 'Autumn'
-            END AS season,
-        COUNT(DISTINCT cb.passenger_uci) AS number_of_passengers
-        FROM confirmed_date_localized_bookings cb
-        INNER JOIN airports ap_orig ON cb.origin_airport = ap_orig.iata
-        INNER JOIN airports ap_dest ON cb.destination_airport = ap_dest.iata
-        WHERE
-            ap_orig.country = 'Netherlands'
-        GROUP BY 
-            destination_country, day_of_week, season
-        ORDER BY 
-            number_of_passengers DESC, season, day_of_week;
-        """ 
-    )
+    result = execute_sql_analysis(analysis_sql_path)
     result.show()
-
-    """
-    TODO:
-    ---most popular destination in network---
-    -DONE which countries are the most popular per season
-    -DONE for each day of the week, over a given time period
-    -DONE only KL flights departing from the Netherlands.
-    -DONE day of the week should be based on the local timezone for the airport.
-    -DONE passenger should only be counted once per flight leg.
-    -DONE We should only count a passenger if their latest booking status is Confirmed.
-
-    -DONE The output should be a table where each row shows :
-        the number of passengers per country, per day of the week, per season. The table should be sorted in descending order by the number of bookings, grouped by season and day of the week.
-    """
 
     """
     functional req:
---
+++ booking_analysis.sql
@@ -0,0 +1,32 @@
+WITH confirmed_date_localized_bookings as(
+    SELECT 
+    b.*, 
+    from_utc_timestamp(b.departure_date, a.tz) AS departure_date_local
+    FROM bookings b
+    INNER JOIN (
+        SELECT passenger_uci, MAX(timestamp) as max_timestamp
+        FROM bookings
+        GROUP BY passenger_uci
+    ) b_max ON b.passenger_uci = b_max.passenger_uci AND b.timestamp = b_max.max_timestamp
+    INNER JOIN airports a on b.origin_airport = a.iata
+    WHERE b.booking_status = 'CONFIRMED' AND b.operating_airline = 'KL'
+)
+SELECT 
+ap_dest.country AS destination_country,
+DATE_FORMAT(to_timestamp(cb.departure_date_local), 'E') AS day_of_week, 
+CASE 
+    WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (12, 1, 2) THEN 'Winter'
+    WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (3, 4, 5) THEN 'Spring'
+    WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (6, 7, 8) THEN 'Summer'
+    ELSE 'Autumn'
+    END AS season,
+COUNT(DISTINCT cb.passenger_uci) AS number_of_passengers
+FROM confirmed_date_localized_bookings cb
+INNER JOIN airports ap_orig ON cb.origin_airport = ap_orig.iata
+INNER JOIN airports ap_dest ON cb.destination_airport = ap_dest.iata
+WHERE
+    ap_orig.country = 'Netherlands'
+GROUP BY 
+    destination_country, day_of_week, season
+ORDER BY 
+    number_of_passengers DESC, season, day_of_week;
