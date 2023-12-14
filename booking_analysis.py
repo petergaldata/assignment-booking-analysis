9ad6bfe665ec36778d274fea35d62bc45b3ad3f1
Peter Gal <153226025+petergaldata@users.noreply.github.com>
2023-12-14 12:09:08 +0100

    booking analytics implemented

--- booking_analysis.py
+++ booking_analysis.py
@@ -46,51 +46,60 @@ if __name__ == "__main__":
 
     result = spark.sql(
         """
+        WITH confirmed_date_localized_bookings as(
+            SELECT 
+            b.*, 
+            from_utc_timestamp(b.departure_date, a.tz) AS departure_date_local
+            FROM bookings b
+            INNER JOIN (
+                SELECT passenger_uci, MAX(timestamp) as max_timestamp
+                FROM bookings
+                GROUP BY passenger_uci
+            ) b_max ON b.passenger_uci = b_max.passenger_uci AND b.timestamp = b_max.max_timestamp
+            INNER JOIN airports a on b.origin_airport = a.iata
+            WHERE b.booking_status = 'CONFIRMED' AND b.operating_airline = 'KL'
+        )
         SELECT 
-        ap_dest.country AS DestinationCountry,
-        DATE_FORMAT(to_timestamp(b.departure_date), 'E') AS DayOfWeek, 
+        ap_dest.country AS destination_country,
+        DATE_FORMAT(to_timestamp(cb.departure_date_local), 'E') AS day_of_week, 
         CASE 
-            WHEN MONTH(to_timestamp(b.departure_date)) IN (12, 1, 2) THEN 'Winter'
-            WHEN MONTH(to_timestamp(b.departure_date)) IN (3, 4, 5) THEN 'Spring'
-            WHEN MONTH(to_timestamp(b.departure_date)) IN (6, 7, 8) THEN 'Summer'
+            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (12, 1, 2) THEN 'Winter'
+            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (3, 4, 5) THEN 'Spring'
+            WHEN MONTH(to_timestamp(cb.departure_date_local)) IN (6, 7, 8) THEN 'Summer'
             ELSE 'Autumn'
-            END AS Season,
-        COUNT(DISTINCT b.passenger_uci) AS NumberOfPassengers
-        FROM bookings b
-        JOIN airports ap_orig ON b.origin_airport = ap_orig.iata
-        JOIN airports ap_dest ON b.destination_airport = ap_dest.iata
-        WHERE 
-            b.operating_airline = 'KL' AND
-            ap_orig.country = 'Netherlands' AND
-            b.booking_status = 'CONFIRMED'
+            END AS season,
+        COUNT(DISTINCT cb.passenger_uci) AS number_of_passengers
+        FROM confirmed_date_localized_bookings cb
+        INNER JOIN airports ap_orig ON cb.origin_airport = ap_orig.iata
+        INNER JOIN airports ap_dest ON cb.destination_airport = ap_dest.iata
+        WHERE
+            ap_orig.country = 'Netherlands'
         GROUP BY 
-            DestinationCountry, DayOfWeek, Season
+            destination_country, day_of_week, season
         ORDER BY 
-            NumberOfPassengers DESC, Season, DayOfWeek;
-        """
+            number_of_passengers DESC, season, day_of_week;
+        """ 
     )
-
     result.show()
 
     """
     TODO:
-    most popular destination in network
-    which countries are the most popular per season
+    ---most popular destination in network---
+    -DONE which countries are the most popular per season
     -DONE for each day of the week, over a given time period
     -DONE only KL flights departing from the Netherlands.
-    day of the week should be based on the local timezone for the airport.
-    passenger should only be counted once per flight leg.
-    We should only count a passenger if their latest booking status is Confirmed.
-    The output should be a table where each row shows :
-    the number of passengers per country, per day of the week, per season. The table should be sorted in descending order by the number of bookings, grouped by season and day of the week.
+    -DONE day of the week should be based on the local timezone for the airport.
+    -DONE passenger should only be counted once per flight leg.
+    -DONE We should only count a passenger if their latest booking status is Confirmed.
+
+    -DONE The output should be a table where each row shows :
+        the number of passengers per country, per day of the week, per season. The table should be sorted in descending order by the number of bookings, grouped by season and day of the week.
     """
 
     """
     functional req:
-    user should be able to specify a local directory 
-    as input for the bookings
+    user should be able to specify a local directory as input for the bookings
     The user should be able to specify a start-date and end-date
-    only KL flights departing from the Netherlands.
     your solution should be able to take as input a directory location on HDFS, containing many files in the same format totaling TBs in size.
     add docker compose to repo
     """
